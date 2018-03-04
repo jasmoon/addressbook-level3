5799eff4a1a16c1825f696515628d7074c2cd045
Toh Jing Jie Jasmund <fourdusk4@gmail.com>
2018-03-04 18:20:23 +0800

    comments added

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,5 +1,9 @@
 package seedu.addressbook.data.person;
 
+/**
+ * A printable interface for objects that require details to be printed in addressbook.
+ * Implementations should guarantee: a printable string
+ */
 public interface Printable {
     /**
      * Returns a concatenated version of the printable strings of each object.
